c11704732471847818035ce6950ab35da051c5e7
Arian Fornaris <developers@boniatillo.com>
2016-09-27 13:41:35 -0400

    Canvas: enable snapping with selection command.

---
+++ source/phasereditor/phasereditor.canvas.ui/src/phasereditor/canvas/ui/handlers/SetSnappingSelectionHandler.java
@@ -0,0 +1,33 @@
+package phasereditor.canvas.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import phasereditor.canvas.ui.editors.CanvasEditor;
+import phasereditor.canvas.ui.editors.SceneSettings;
+import phasereditor.canvas.ui.shapes.ISpriteNode;
+
+public class SetSnappingSelectionHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		CanvasEditor editor = (CanvasEditor) HandlerUtil.getActiveEditor(event);
+		SceneSettings settings = editor.getCanvas().getSettingsModel();
+
+		Object node = HandlerUtil.getCurrentStructuredSelection(event).getFirstElement();
+
+		if (node != null && node instanceof ISpriteNode) {
+			ISpriteNode obj = (ISpriteNode) node;
+			settings.setStepWidth((int) obj.getControl().getTextureWidth());
+			settings.setStepHeight((int) obj.getControl().getTextureHeight());
+			settings.setEnableStepping(true);
+			editor.getCanvas().getBackGridPane().repaint();
+		}
+
+		return null;
+	}
+
+}
