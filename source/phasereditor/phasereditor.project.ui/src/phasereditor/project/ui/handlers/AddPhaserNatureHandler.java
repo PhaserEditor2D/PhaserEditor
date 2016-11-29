92349fda061c3391a8e1979f0693c85ca3e29069
Arian Fornaris <developers@boniatillo.com>
2016-11-29 15:53:30 -0500

    Project: add/remove phaser nature commands.

---
+++ source/phasereditor/phasereditor.project.ui/src/phasereditor/project/ui/handlers/AddPhaserNatureHandler.java
@@ -0,0 +1,35 @@
+package phasereditor.project.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import phasereditor.project.core.PhaserProjectNature;
+
+public class AddPhaserNatureHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IStructuredSelection sel = HandlerUtil.getCurrentStructuredSelection(event);
+		for (Object obj : sel.toArray()) {
+			if (obj instanceof IProject) {
+				try {
+					IProject project = (IProject) obj;
+					if (!PhaserProjectNature.hasNature(project)) {
+						PhaserProjectNature.addPhaserNature(project, new NullProgressMonitor());
+					}
+				} catch (CoreException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+
+		return null;
+	}
+
+}
