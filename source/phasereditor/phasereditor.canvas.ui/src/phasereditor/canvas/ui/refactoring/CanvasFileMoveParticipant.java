5366f9549b0eba029074d94440a0022d75b10c1f
Arian Fornaris <developers@boniatillo.com>
2017-02-14 12:28:18 -0500

    Refactoring: update not opened files after prefab move.

--- source/phasereditor/phasereditor.canvas.ui/src/phasereditor/canvas/ui/refactoring/CanvasFileMoveParticipant.java
+++ source/phasereditor/phasereditor.canvas.ui/src/phasereditor/canvas/ui/refactoring/CanvasFileMoveParticipant.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
@@ -18,10 +17,12 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.MoveArguments;
 import org.eclipse.ltk.core.refactoring.participants.MoveParticipant;
 import org.eclipse.ltk.core.refactoring.resource.MoveResourceChange;
+import org.eclipse.text.edits.ReplaceEdit;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -31,6 +32,7 @@ import phasereditor.canvas.core.CanvasFile;
 import phasereditor.canvas.core.CanvasType;
 import phasereditor.canvas.core.Prefab;
 import phasereditor.canvas.ui.CanvasUI;
+import phasereditor.canvas.ui.editors.CanvasEditor;
 import phasereditor.ui.PhaserEditorUI;
 
 public class CanvasFileMoveParticipant extends MoveParticipant {
@@ -103,19 +105,17 @@ public class CanvasFileMoveParticipant extends MoveParticipant {
 
 		IContainer dst = (IContainer) args.getDestination();
 
-		if (files.size() == 1) {
-			return new MoveResourceChange(files.get(0), dst);
-		}
-
-		CompositeChange change = new CompositeChange(
+		CompositeChange changes = new CompositeChange(
 				"Delete " + files.size() + " files derived from " + _file.getFullPath().toPortableString());
 
 		for (IFile file : files) {
-			change.add(new MoveResourceChange(file, dst));
+			changes.add(new MoveResourceChange(file, dst));
 		}
 
+		String srcPath = _file.getProjectRelativePath().toPortableString();
+
 		List<CanvasFile> cfiles = CanvasCore.getCanvasFileCache().getProjectData(_file.getProject());
-		
+
 		for (CanvasFile cfile : cfiles) {
 			IFile file = cfile.getFile();
 
@@ -123,18 +123,42 @@ public class CanvasFileMoveParticipant extends MoveParticipant {
 				continue;
 			}
 
-			boolean inEditor = PhaserEditorUI.forEachOpenFileEditor(file, editor -> {
-				out.println("Missing to check " + editor);
+			boolean[] inEditor = { false };
+			PhaserEditorUI.forEachOpenFileEditor(file, editor -> {
+				if (editor instanceof CanvasEditor) {
+					inEditor[0] = true;
+					out.println("Missing to check " + editor);
+				}
 			});
 
-			if (!inEditor) {
-				
+			if (!inEditor[0]) {
 				IFile dstFile = dst.getFile(new Path(_file.getName()));
 				String dstPath = dstFile.getProjectRelativePath().toPortableString();
-				
+
 				try (InputStream contents = file.getContents()) {
-					JSONObject obj = new JSONObject(new JSONTokener(contents));
-//					replaceGropup(world,)
+					JSONObject data = new JSONObject(new JSONTokener(contents));
+
+					boolean modified = false;
+
+					if (data.has("prefab-table")) {
+						JSONObject table = data.getJSONObject("prefab-table");
+						for (String id : table.keySet()) {
+							String fname = table.getString(id);
+							if (srcPath.equals(fname)) {
+								table.put(id, dstPath);
+								modified = true;
+							}
+						}
+					}
+
+					if (modified) {
+						String content = data.toString(2);
+						TextFileChange textChange = new TextFileChange("Update prefab file", file);
+						int length = (int) file.getLocation().toFile().length();
+						textChange.setEdit(new ReplaceEdit(0, length, content));
+						changes.add(textChange);
+					}
+
 				} catch (IOException e) {
 					CanvasUI.logError(e);
 				}
@@ -144,7 +168,7 @@ public class CanvasFileMoveParticipant extends MoveParticipant {
 		// TextFileChange change2 = new TextFileChange("pepe", _file);
 		// change2.setEdit(new ReplaceEdit(0, 0, getName()));
 
-		return change;
+		return changes;
 	}
 
 }
