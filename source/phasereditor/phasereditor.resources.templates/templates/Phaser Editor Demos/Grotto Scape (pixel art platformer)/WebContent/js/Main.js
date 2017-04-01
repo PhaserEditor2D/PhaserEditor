42e0d10dcf65cce5b818fb778bb7dbfa7682c0e2
Arian Fornaris <developers@boniatillo.com>
2017-04-01 14:57:21 -0400

    Project templates: Grotto Scape done.

--- source/phasereditor/phasereditor.resources.templates/templates/Phaser Editor Demos/Grotto Scape (pixel art platformer)/WebContent/js/Main.js
+++ source/phasereditor/phasereditor.resources.templates/templates/Phaser Editor Demos/Grotto Scape (pixel art platformer)/WebContent/js/Main.js
@@ -1,9 +1,11 @@
 window.onload = function() {
-	// Create your Phaser game and inject it into an auto-created canvas.
-	// We did it in a window.onload event, but you can do it anywhere (requireJS
-	// load, anonymous function, jQuery dom ready, - whatever floats your boat)
-	var game = new Phaser.Game(240, 208, Phaser.CANVAS);
+	var game = new Phaser.Game(240, 208, Phaser.AUTO);
 
-	game.state.add("level", Level);
-	game.state.start("level");
+	// Add the States your game has.
+	// game.state.add("Boot", Boot);
+	// game.state.add("Menu", Menu);
+	// game.state.add("Preload", Preload);
+	game.state.add("Level", Level);
+
+	game.state.start("Level");
 };
