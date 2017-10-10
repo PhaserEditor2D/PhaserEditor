e4d30a55c312fab54012abdb27f0923f5b0ea7c7
Arian Fornaris <developers@boniatillo.com>
2017-10-10 05:12:12 -0400

    Move class to the right package.

--- source/phasereditor/phasereditor.canvas.ui/src/phasereditor/canvas/ui/handlers/AddTextHandler.java
+++ source/phasereditor/phasereditor.canvas.ui/src/phasereditor/canvas/ui/handlers/AddTextHandler.java
@@ -1,3 +1,4 @@
+package phasereditor.canvas.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
