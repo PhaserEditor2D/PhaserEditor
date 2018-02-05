dce338e7ec3b1d8d97317ac998e5cd6e144362a3
Arian Fornaris <developers@phasereditor2d.com>
2018-02-05 18:18:07 -0500

    Tilemap csv preview: write selected tiles to text field. Copy/paste.

--- source/phasereditor/phasereditor.assetpack.ui/src/phasereditor/assetpack/ui/preview/TilemapCSVAssetPreviewComp.java
+++ source/phasereditor/phasereditor.assetpack.ui/src/phasereditor/assetpack/ui/preview/TilemapCSVAssetPreviewComp.java
@@ -21,555 +21,132 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 package phasereditor.assetpack.ui.preview;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.Separator;
-import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.window.Window;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.events.MouseListener;
-import org.eclipse.swt.events.MouseMoveListener;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.FontMetrics;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IMemento;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import org.eclipse.wb.swt.ResourceManager;
 
-import phasereditor.assetpack.core.AssetPackCore;
-import phasereditor.assetpack.core.ImageAssetModel;
 import phasereditor.assetpack.core.TilemapAssetModel;
-import phasereditor.assetpack.ui.SelectTextureDialog;
-import phasereditor.assetpack.ui.TextureListContentProvider;
-import phasereditor.ui.EditorSharedImages;
-import phasereditor.ui.IEditorSharedImages;
-import phasereditor.ui.ImageCanvas_Zoom_1_1_Action;
-import phasereditor.ui.ImageCanvas_Zoom_FitWindow_Action;
-import phasereditor.ui.PhaserEditorUI;
-import phasereditor.ui.ZoomCanvas;
 
 /**
  * @author arian
  *
  */
-public class TilemapCSVAssetPreviewComp extends ZoomCanvas implements MouseMoveListener, MouseListener, KeyListener {
+public class TilemapCSVAssetPreviewComp extends Composite implements ISelectionChangedListener {
+	Text _selectedFramesText;
+	private TilemapCanvas _tilemapCanvas;
+	// 54,55,56,57,59,60,61,62,63,65,66,69,70,75,104,105,108
 
-	private static final String MEMENTO_IMAGE_KEY = "phasereditor.assetpack.ui.tilemap.csv.image";
-	private static final String MEMENTO_TILE_WIDTH = "phasereditor.assetpack.ui.tilemap.csv.tileWidth";
-	private static final String MEMENTO_TILE_HEIGHT = "phasereditor.assetpack.ui.tilemap.csv.tileHeight";
-
-	private TilemapAssetModel _model;
-	private Point _imageSize = new Point(1, 1);
-	private int _tileWidth = 32;
-	private int _tileHeight = 32;
-	private Color[] _colors;
-	protected ImageAssetModel _imageModel;
-	private Image _tileSetImage;
-	private Image _renderImage;
-	private String _initialImageRef;
-	private int _mouseX;
-	private int _mouseY;
-	private List<Point> _selectedCells;
-	private int _mouseMapX;
-	private int _mouseMapY;
-
+	/**
+	 * Create the composite.
+	 * 
+	 * @param parent
+	 * @param style
+	 */
 	public TilemapCSVAssetPreviewComp(Composite parent, int style) {
 		super(parent, style);
+		setLayout(new GridLayout(1, false));
 
-		_selectedCells = new ArrayList<Point>() {
-			private static final long serialVersionUID = 1L;
+		_tilemapCanvas = new TilemapCanvas(this, SWT.NONE);
+		_tilemapCanvas.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 
+		Composite composite = new Composite(this, SWT.NONE);
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		composite.setLayout(new GridLayout(4, false));
+
+		_selectedFramesText = new Text(composite, SWT.BORDER);
+		_selectedFramesText.setToolTipText("The selected tile indexes, sepparated by a colon.");
+		_selectedFramesText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		Button btnNewButton_1 = new Button(composite, SWT.NONE);
+		btnNewButton_1.setToolTipText("Copy tile indexes.");
+		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public boolean add(Point e) {
-				if (!contains(e)) {
-					return super.add(e);
-				}
-				return false;
+			public void widgetSelected(SelectionEvent e) {
+				_selectedFramesText.selectAll();
+				_selectedFramesText.copy();
+				_selectedFramesText.setSelection(0, 0);
 			}
-		};
-
-		addPaintListener(this);
-		addMouseMoveListener(this);
-		addMouseListener(this);
-		addKeyListener(this);
-	}
-
-	public void generateColors(int n) {
-		_colors = new Color[n];
-		for (int i = 0; i < n; i++) {
-			java.awt.Color c = java.awt.Color.getHSBColor((float) i / (float) n, 0.85f, 1.0f);
-			_colors[i] = SWTResourceManager.getColor(c.getRed(), c.getGreen(), c.getBlue());
-		}
-	}
-
-	public void setModel(TilemapAssetModel model) {
-		if (model != null) {
-			model.buildTilemap();
-		}
-
-		_model = model;
-
-		int[][] map = _model == null ? null : _model.getCsvData();
-		if (map != null) {
-			int max = 0;
-			for (int i = 0; i < map.length; i++) {
-				for (int j = 0; j < map[i].length; j++) {
-					max = Math.max(map[i][j], max);
-				}
-			}
-
-			generateColors(max);
-
-			updateImageSize();
-		}
-
-		if (_initialImageRef != null) {
-			if (model != null) {
-				try {
-					JSONObject obj = new JSONObject(new JSONTokener(_initialImageRef));
-					Object asset = AssetPackCore.findAssetElement(model.getPack().getFile().getProject(), obj);
-					if (asset instanceof ImageAssetModel) {
-						_imageModel = (ImageAssetModel) asset;
-						buildTilesetImage();
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			_initialImageRef = null;
-		}
-
-		buildMapImage();
-
-		reset();
-	}
-
-	void updateImageSize() {
-		if (_model != null) {
-			int[][] map = _model.getCsvData();
-			if (map[0].length > 0) {
-				_imageSize = new Point(map[0].length * getTileWidth(), map.length * getTileHeight());
-				return;
-			}
-		}
-		_imageSize = new Point(1, 1);
-	}
-
-	public void setImageModel(ImageAssetModel imageModel) {
-		_imageModel = imageModel;
-
-		buildTilesetImage();
-
-		buildMapImage();
-
-		redraw();
-	}
-
-	private void buildTilesetImage() {
-		Image old = _tileSetImage;
-
-		if (_imageModel == null) {
-			_tileSetImage = null;
-		} else {
-			_tileSetImage = new Image(getDisplay(), _imageModel.getUrlFile().getLocation().toFile().getAbsolutePath());
-		}
-
-		if (old != null) {
-			old.dispose();
-		}
-	}
-
-	void buildMapImage() {
-		if (_model != null && _tileSetImage != null) {
-
-			int[][] map = _model.getCsvData();
-
-			if (map != null && map.length > 0) {
-
-				Point size = getImageSize();
-
-				ImageData srcData = _tileSetImage.getImageData();
-				ImageData data = new ImageData(size.x, size.y, srcData.depth, srcData.palette);
-
-				Image image = new Image(getDisplay(), data);
-
-				GC gc = new GC(image);
-
-				for (int i = 0; i < map.length; i++) {
-					int[] row = map[i];
-
-					for (int j = 0; j < row.length; j++) {
-
-						int frame = map[i][j];
-
-						int w = getTileWidth();
-						int h = getTileHeight();
-						int x = j * getTileWidth();
-						int y = i * getTileHeight();
-
-						Rectangle b = _tileSetImage.getBounds();
-						int srcX = frame * _tileWidth % b.width;
-						int srcY = frame * _tileWidth / b.width * _tileHeight;
-
-						try {
-							gc.drawImage(_tileSetImage, srcX, srcY, _tileWidth, _tileHeight, x, y, w, h);
-						} catch (IllegalArgumentException e) {
-							gc.drawText("Invalid parameters. Please check the tiles size.", 10, 10);
-						}
-					}
-				}
-
-				Image old = _renderImage;
-				_renderImage = image;
-
-				if (old != null) {
-					old.dispose();
-				}
-
-				_renderImage = image;
-
-				return;
-			}
-		}
-	}
-
-	@Override
-	public void dispose() {
-		super.dispose();
-		if (_renderImage != null) {
-			_renderImage.dispose();
-		}
-	}
-
-	public TilemapAssetModel getModel() {
-		return _model;
-	}
-
-	@Override
-	protected Point getImageSize() {
-		return _imageSize;
-	}
-
-	@Override
-	public void paintControl(PaintEvent e) {
-		GC gc = e.gc;
-		gc.setAdvanced(true);
-
-		if (_model != null) {
-
-			int[][] map = _model.getCsvData();
-
-			if (map != null && map.length > 0) {
-
-				ZoomCalculator calc = calc();
-
-				float scale = calc.scale;
-				float offX = calc.offsetX;
-				float offY = calc.offsetY;
-
-				if (_renderImage == null) {
-
-					for (int i = 0; i < map.length; i++) {
-						int[] row = map[i];
-
-						for (int j = 0; j < row.length; j++) {
-
-							int frame = map[i][j];
-
-							int w = (int) (getTileWidth() * scale);
-							int h = (int) (getTileHeight() * scale);
-							int x = (int) (j * getTileWidth() * scale + offX);
-							int y = (int) (i * getTileHeight() * scale + offY);
-
-							if (_tileSetImage == null) {
-								// paint map with colors
-								Color c = _colors[frame % _colors.length];
-								gc.setBackground(c);
-								gc.fillRectangle(x, y, w + 1, h + 1);
-							}
-						}
-					}
-				} else {
-					Rectangle b = _renderImage.getBounds();
-					gc.drawImage(_renderImage, 0, 0, b.width, b.height, (int) offX, (int) offY, (int) (b.width * scale),
-							(int) (b.height * scale));
-				}
-
-				Color borderColor = PhaserEditorUI.get_pref_Preview_Spritesheet_borderColor();
-				Color labelsColor = PhaserEditorUI.get_pref_Preview_Spritesheet_labelsColor();
-				Color colorBlack = getDisplay().getSystemColor(SWT.COLOR_BLACK);
-				Color selectionColor = PhaserEditorUI.get_pref_Preview_Spritesheet_selectionColor();
-
-				for (Point p : _selectedCells) {
-					gc.setAlpha(100);
-					gc.setBackground(selectionColor);
-					gc.fillRectangle((int) (offX + p.x * _tileWidth * scale), (int) (offY + p.y * _tileHeight * scale),
-							(int) (_tileWidth * scale), (int) (_tileHeight * scale));
-					gc.setAlpha(255);
-				}
-
-				try {
-
-					int frame = map[_mouseMapY][_mouseMapX];
-
-					gc.setForeground(borderColor);
-
-					int cellY = (int) (offY + _mouseMapY * _tileHeight * scale);
-					int cellX = (int) (offX + _mouseMapX * _tileWidth * scale);
-					int cellW = (int) (_tileWidth * scale);
-					int cellH = (int) (_tileHeight * scale);
-
-					gc.drawRectangle(cellX, cellY, cellW, cellH);
-
-					FontMetrics fm = gc.getFontMetrics();
-					int fh = fm.getHeight() + 3;
-					int fw = fm.getAverageCharWidth() + 3;
-
-					int y;
-					int x;
-
-					Rectangle b = getBounds();
-
-					if (_mouseY < b.height / 2) {
-						y = cellY + cellH;
-					} else {
-						y = cellY - fh;
-					}
-
-					if (_mouseX < b.width / 2) {
-						x = cellX + cellW;
-					} else {
-						x = cellX - fw;
-					}
-
-					String label = Integer.toString(frame);
-					gc.setForeground(colorBlack);
-					gc.drawString(label, x - 1, y + 1, true);
-					gc.setForeground(colorBlack);
-					gc.drawString(label, x + 1, y - 1, true);
-					gc.setForeground(labelsColor);
-					gc.drawString(label, x, y, true);
-				} catch (Exception e2) {
-					// nothing
-				}
-
-				return;
-			}
-		}
-
-		gc.drawText("Missing map data", 10, 10);
-	}
-
-	public void createToolBar(IToolBarManager toolbar) {
-		toolbar.add(new ImageCanvas_Zoom_1_1_Action(this));
-		toolbar.add(new ImageCanvas_Zoom_FitWindow_Action(this));
-
-		toolbar.add(new Separator());
-
-		toolbar.add(createSelectTilesetImageAction());
-		toolbar.add(createSetTileSizeAction());
-	}
-
-	private Action createSetTileSizeAction() {
-		return new Action("Set the tiles size") {
-			{
-				setImageDescriptor(EditorSharedImages.getImageDescriptor(IEditorSharedImages.IMG_SETTINGS));
-			}
-
+		});
+		btnNewButton_1.setImage(ResourceManager.getPluginImage("org.eclipse.ui", "/icons/full/etool16/copy_edit.png"));
+
+		Button btnNewButton_2 = new Button(composite, SWT.NONE);
+		btnNewButton_2.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void run() {
-				InputDialog dlg = new InputDialog(getShell(), "Tile Size", "Set the tile size (eg. 16x16)",
-						getTileWidth() + "x" + getTileHeight(), new IInputValidator() {
-
-							@Override
-							public String isValid(String str) {
-								try {
-									String[] tuple = str.split("x");
-									Integer.parseInt(tuple[0]);
-									Integer.parseInt(tuple[1]);
-								} catch (Exception e) {
-									return "Invalid format. Please enter something like 16x16";
-								}
-
-								return null;
-							}
-						});
-				if (dlg.open() == Window.OK) {
-					String result = dlg.getValue();
-					String[] tuple = result.split("x");
-					setTileWidth(Integer.parseInt(tuple[0]));
-					setTileHeight(Integer.parseInt(tuple[1]));
-					updateImageSize();
-					buildMapImage();
-					redraw();
-				}
+			public void widgetSelected(SelectionEvent e) { 
+				_selectedFramesText.paste();
 			}
-		};
-	}
+		});
+		btnNewButton_2.setToolTipText("Paste tile indexes.");
+		btnNewButton_2.setImage(ResourceManager.getPluginImage("org.eclipse.ui", "/icons/full/etool16/paste_edit.png"));
 
-	private Action createSelectTilesetImageAction() {
-		return new Action("Select the tileset image.") {
-
-			{
-				setImageDescriptor(EditorSharedImages.getImageDescriptor(IEditorSharedImages.IMG_IMAGE));
-			}
-
+		Button btnNewButton = new Button(composite, SWT.NONE);
+		btnNewButton.setToolTipText("Select all entered tile indexes in the map.");
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void run() {
-				TilemapAssetModel model = getModel();
-
-				if (model == null) {
-					return;
-				}
-
-				SelectTextureDialog dlg = new SelectTextureDialog(getShell(), "Tileset Image");
-				dlg.setContentProvider(new TextureListContentProvider() {
-					@Override
-					protected boolean acceptAsset(Object assetKey) {
-						return assetKey instanceof ImageAssetModel;
-					}
-				});
-				dlg.setProject(model.getPack().getFile().getProject());
-				if (dlg.open() == Window.OK) {
-					ImageAssetModel image = (ImageAssetModel) dlg.getSelection().getFirstElement();
-					setImageModel(image);
-				}
+			public void widgetSelected(SelectionEvent e) {
+				loadSelectedFramesFromText();
 			}
-		};
+		});
+		btnNewButton.setImage(ResourceManager.getPluginImage("phasereditor.ui", "icons/accept.png"));
+
+		afterCreateWidgets();
+
 	}
 
-	public int getTileWidth() {
-		return _tileWidth;
+	private void afterCreateWidgets() {
+		_tilemapCanvas.addSelectionChangedListener(this);
 	}
 
-	public void setTileWidth(int tileWidth) {
-		_tileWidth = tileWidth;
-	}
-
-	public int getTileHeight() {
-		return _tileHeight;
-	}
-
-	public void setTileHeight(int tileHeight) {
-		_tileHeight = tileHeight;
-	}
-
-	public void initState(IMemento memento) {
-		_initialImageRef = memento.getString(MEMENTO_IMAGE_KEY);
-
-		{
-			Integer value = memento.getInteger(MEMENTO_TILE_WIDTH);
-			_tileWidth = value == null ? _tileWidth : value.intValue();
-		}
-
-		{
-			Integer value = memento.getInteger(MEMENTO_TILE_HEIGHT);
-			_tileHeight = value == null ? _tileHeight : value.intValue();
-		}
-
-		if (_initialImageRef != null) {
-			setModel(_model);
-		}
-	}
-
-	public void saveState(IMemento memento) {
-		if (_imageModel != null) {
-			JSONObject ref = AssetPackCore.getAssetJSONReference(_imageModel);
-			memento.putString(MEMENTO_IMAGE_KEY, ref.toString());
-		}
-
-		memento.putInteger(MEMENTO_TILE_WIDTH, _tileWidth);
-		memento.putInteger(MEMENTO_TILE_HEIGHT, _tileHeight);
-	}
-
-	@Override
-	public void mouseMove(MouseEvent e) {
-		_mouseX = e.x;
-		_mouseY = e.y;
-
-		ZoomCalculator calc = calc();
-
-		_mouseMapX = (int) ((_mouseX - calc.offsetX) / calc.scale / _tileWidth);
-		_mouseMapY = (int) ((_mouseY - calc.offsetY) / calc.scale / _tileHeight);
-
-		redraw();
-	}
-
-	@Override
-	public void mouseDoubleClick(MouseEvent e) {
-		if (e.button == 1) {
-			selectAllFrames();
-		}
-	}
-
-	private void selectAllFrames() {
+	@SuppressWarnings("boxing")
+	protected void loadSelectedFramesFromText() {
+		String text = _selectedFramesText.getText();
 		try {
-			boolean selected = _selectedCells.contains(new Point(_mouseMapX, _mouseMapY));
-			int[][] map = _model.getCsvData();
-			int frame = map[_mouseMapY][_mouseMapX];
-			for (int i = 0; i < map.length; i++) {
-				int[] row = map[i];
-				for (int j = 0; j < row.length; j++) {
-					if (frame == map[i][j]) {
-						if (selected) {
-							_selectedCells.remove(new Point(j, i));
-						} else {
-							_selectedCells.add(new Point(j, i));
-						}
-					}
-				}
-			}
-			redraw();
-		} catch (Exception e2) {
+			List<Integer> frames = Arrays.stream(text.split(",")).map(t -> Integer.parseInt(t))
+					.collect(Collectors.toList());
+			_tilemapCanvas.selectAllFrames(frames);
+		} catch (Exception e) {
 			// nothing
 		}
 	}
 
-	@Override
-	public void mouseDown(MouseEvent e) {
-		if (e.button == 1) {
-			Point p = new Point(_mouseMapX, _mouseMapY);
-			if (_selectedCells.contains(p)) {
-				_selectedCells.remove(p);
-			} else {
-				_selectedCells.add(p);
-			}
-			redraw();
-		}
+	public void setModel(TilemapAssetModel model) {
+		_tilemapCanvas.setModel(model);
 	}
 
-	@Override
-	public void mouseUp(MouseEvent e) {
-		// nothing
+	public void initState(IMemento memento) {
+		_tilemapCanvas.initState(memento);
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// nothing
+	public void saveState(IMemento memento) {
+		_tilemapCanvas.saveState(memento);
 	}
 
+	public void createToolBar(IToolBarManager toolbar) {
+		_tilemapCanvas.createToolBar(toolbar);
+	}
+
+	@SuppressWarnings("boxing")
 	@Override
-	public void keyReleased(KeyEvent e) {
-		if (e.keyCode == SWT.ESC) {
-			_selectedCells.clear();
-			redraw();
-		} else if (e.character == ' ') {
-			selectAllFrames();
-		}
+	public void selectionChanged(SelectionChangedEvent event) {
+		List<Point> sel = _tilemapCanvas.getSelectedCells();
+		String text = sel.stream().map(p -> _tilemapCanvas.getModel().getCsvData()[p.y][p.x]).distinct().sorted()
+				.map(o -> o.toString()).collect(Collectors.joining(","));
+		_selectedFramesText.setText(text);
 	}
 
 }
