492dadfb07154fc16e72fe9e366b1e9eab23f632
Arian Fornaris <developers@phasereditor2d.com>
2018-06-25 08:52:25 -0400

    (v2) Phaser Exampels view: show workbench selection.

--- source/v2/phasereditor/phasereditor.inspect.ui/src/phasereditor/inspect/ui/views/PhaserExamplesView.java
+++ source/v2/phasereditor/phasereditor.inspect.ui/src/phasereditor/inspect/ui/views/PhaserExamplesView.java
@@ -21,13 +21,25 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 package phasereditor.inspect.ui.views;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.Adapters;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.dialogs.FilteredTree;
 import org.eclipse.ui.part.ViewPart;
 
 import phasereditor.inspect.core.InspectCore;
+import phasereditor.inspect.core.examples.PhaserExampleCategoryModel;
+import phasereditor.inspect.core.examples.PhaserExampleModel;
 import phasereditor.inspect.ui.TemplateContentProvider;
 import phasereditor.inspect.ui.TemplateLabelProvider;
 import phasereditor.ui.PatternFilter2;
@@ -36,7 +48,7 @@ import phasereditor.ui.PatternFilter2;
  * @author arian
  *
  */
-public class PhaserExamplesView extends ViewPart {
+public class PhaserExamplesView extends ViewPart implements ISelectionListener {
 
 	public static final String ID = "phasereditor.inspect.ui.views.phaserExamples"; //$NON-NLS-1$
 	private TreeViewer _viewer;
@@ -55,14 +67,70 @@ public class PhaserExamplesView extends ViewPart {
 		afterCreateWidgets();
 	}
 
+	@Override
+	public void dispose() {
+
+		getViewSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(this);
+
+		super.dispose();
+	}
+
 	private void afterCreateWidgets() {
 		_viewer.setInput(InspectCore.getPhaserExamplesRepoModel());
 		getViewSite().setSelectionProvider(_viewer);
+		getViewSite().getWorkbenchWindow().getSelectionService().addSelectionListener(this);
 	}
 
 	@Override
 	public void setFocus() {
 		_viewer.getControl().setFocus();
+	}
+
+	@Override
+	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			Object elem = ((IStructuredSelection) selection).getFirstElement();
+
+			if (elem == null) {
+				return;
+			}
+
+			elem = Adapters.adapt(elem, PhaserExampleModel.class);
+
+			if (elem == null) {
+				return;
+			}
+
+			Object[] path = buildPath((PhaserExampleModel) elem);
+			_viewer.reveal(new TreePath(path));
+			_viewer.setSelection(new StructuredSelection(elem));
+		}
+	}
+
+	private Object[] buildPath(PhaserExampleModel elem) {
+		List<Object> path = new ArrayList<>();
+
+		buildPath(elem, path);
+
+		return path.toArray();
+	}
+
+	private void buildPath(Object elem, List<Object> path) {
+		Object parent = null;
+		if (elem instanceof PhaserExampleModel) {
+			parent = ((PhaserExampleModel) elem).getCategory();
+		} else {
+			parent = ((PhaserExampleCategoryModel) elem).getParentCategory();
+		}
+
+		if (parent != null) {
+
+			buildPath(parent, path);
+
+			path.add(parent);
+		}
+
+		path.add(elem);
 	}
 
 }
