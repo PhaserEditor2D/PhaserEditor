1581212e4557dca0848a6c3449d3d8fbb2451052
Arian Fornaris <developers@phasereditor2d.com>
2018-08-07 23:32:01 -0400

    Remove some warnings.

--- source/v2/phasereditor/phasereditor.assetpack.ui/src/phasereditor/assetpack/ui/refactorings/DeleteAssetChange.java
+++ source/v2/phasereditor/phasereditor.assetpack.ui/src/phasereditor/assetpack/ui/refactorings/DeleteAssetChange.java
@@ -27,7 +27,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.swt.widgets.Display;
