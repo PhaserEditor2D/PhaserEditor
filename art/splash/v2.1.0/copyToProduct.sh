46fed1ec5252acf53b81200dff4f1a65745fe4b3
Arian Fornaris <developers@phasereditor2d.com>
2019-03-26 10:15:56 -0400

    More about splah and launcher images.

---
+++ art/splash/v2.1.0/copyToProduct.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp splash.bmp ~/Documents/PhaserEditor/Public/source/v2/phasereditor/phasereditor.ide/
+optipng launcher-background.png
+cp launcher-background.png ~/Documents/PhaserEditor/Public/source/v2/phasereditor/phasereditor.ide/launcher/background.png
