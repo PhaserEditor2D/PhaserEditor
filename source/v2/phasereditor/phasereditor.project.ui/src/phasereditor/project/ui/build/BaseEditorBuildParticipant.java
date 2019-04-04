96a55c7d5f6692ffd5f94636c207e4541df17583
Arian Fornaris <developers@phasereditor2d.com>
2019-04-04 07:00:27 -0400

    Do not build editor at startup.

--- source/v2/phasereditor/phasereditor.project.ui/src/phasereditor/project/ui/build/BaseEditorBuildParticipant.java
+++ source/v2/phasereditor/phasereditor.project.ui/src/phasereditor/project/ui/build/BaseEditorBuildParticipant.java
@@ -57,7 +57,7 @@ public abstract class BaseEditorBuildParticipant<T extends EditorPart> implement
 
 	@Override
 	public void startupOnInitialize(IProject project, Map<String, Object> env) {
-		rebuildEditors();
+		// rebuildEditors();
 	}
 
 	private void rebuildEditors() {
