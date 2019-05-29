b58c341444c17cfab3b3b1c4f1d055be01f02573
Arian Fornaris <developers@phasereditor2d.com>
2019-05-29 14:00:35 -0400

    #118: (v1) Spelling mistake on Atlas Generator Tool tip.

--- source/phasereditor/phasereditor.atlas.ui/src/phasereditor/atlas/ui/editors/AtlasGeneratorEditor.java
+++ source/phasereditor/phasereditor.atlas.ui/src/phasereditor/atlas/ui/editors/AtlasGeneratorEditor.java
@@ -275,7 +275,7 @@ public class AtlasGeneratorEditor extends EditorPart implements IEditorSharedIma
 		_settingsAction
 				.setImageDescriptor(ResourceManager.getPluginImageDescriptor("phasereditor.ui", "icons/wrench.png"));
 
-		_layoutAction = new Action("Show/hide the legft side panel.", IAction.AS_CHECK_BOX) {
+		_layoutAction = new Action("Show/hide the left side panel.", IAction.AS_CHECK_BOX) {
 			@Override
 			public void run() {
 				changeLayout();
