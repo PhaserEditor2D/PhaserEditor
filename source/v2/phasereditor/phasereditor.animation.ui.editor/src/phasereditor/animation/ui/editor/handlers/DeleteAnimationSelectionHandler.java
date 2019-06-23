88608e785deb8cd9d7b8936c1e0277cfc982cbe6
Arian Fornaris <developers@phasereditor2d.com>
2019-06-23 15:10:30 -0400

    Animations editor: simplfies the detele handler.

--- source/v2/phasereditor/phasereditor.animation.ui.editor/src/phasereditor/animation/ui/editor/handlers/DeleteAnimationSelectionHandler.java
+++ source/v2/phasereditor/phasereditor.animation.ui.editor/src/phasereditor/animation/ui/editor/handlers/DeleteAnimationSelectionHandler.java
@@ -1,16 +1,11 @@
 package phasereditor.animation.ui.editor.handlers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import phasereditor.animation.ui.editor.AnimationsEditor;
-import phasereditor.assetpack.core.animations.AnimationFrameModel;
-import phasereditor.assetpack.core.animations.AnimationModel;
 
 public class DeleteAnimationSelectionHandler extends AbstractHandler {
 
@@ -18,32 +13,7 @@ public class DeleteAnimationSelectionHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		var editor = (AnimationsEditor) HandlerUtil.getActiveEditor(event);
-
-		var sel = HandlerUtil.getCurrentStructuredSelection(event);
-
-		List<AnimationModel> animations = new ArrayList<>();
-		List<AnimationFrameModel> frames = new ArrayList<>();
-
-		for (var obj : sel.toArray()) {
-			if (obj instanceof AnimationModel) {
-				animations.add((AnimationModel) obj);
-			} else if (obj instanceof AnimationFrameModel) {
-				frames.add((AnimationFrameModel) obj);
-			}
-		}
-
-		if (frames.isEmpty()) {
-			var activePart = HandlerUtil.getActivePart(event);
-			// if the active part is the editor it means we are trying to delete the current
-			// animation, but it is not what we want to do.
-			if (activePart != editor) {
-				if (!animations.isEmpty()) {
-					editor.deleteAnimations(animations);
-				}
-			}
-		} else {
-			editor.deleteFrames(frames);
-		}
+		editor.getDeleteAction().run();
 
 		return null;
 	}
