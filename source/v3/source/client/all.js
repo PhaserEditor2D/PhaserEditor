c7d7f7a1d6bae733d64da27ccef990a55c877aee
Arian Fornaris <developers@phasereditor2d.com>
2019-10-07 23:50:18 -0400

    (v3) Welcome banner in devtools.

--- source/v3/source/client/all.js
+++ source/v3/source/client/all.js
@@ -1,6 +1,9 @@
 var phasereditor2d;
 (function (phasereditor2d) {
+    phasereditor2d.VER = "3.0.0";
     async function main() {
+        console.log(`%c %c Phaser Editor 2D %c v${phasereditor2d.VER} %c %c https://phasereditor2d.com `, "background-color:red", "background-color:#3f3f3f;color:whitesmoke", "background-color:orange;color:black", "background-color:red", "background-color:silver");
+        console.log("");
         console.log("Preloading UI resources");
         await phasereditor2d.ui.controls.Controls.preload();
         console.log("Starting the workbench");
--- source/v3/source/client/main.ts
+++ source/v3/source/client/main.ts
@@ -1,6 +1,18 @@
 namespace phasereditor2d {
 
+    export const VER = "3.0.0";
+
     export async function main() {
+
+        console.log(`%c %c Phaser Editor 2D %c v${VER} %c %c https://phasereditor2d.com `,
+            "background-color:red",
+            "background-color:#3f3f3f;color:whitesmoke",
+            "background-color:orange;color:black",
+            "background-color:red",
+            "background-color:silver",
+        );
+
+        console.log("");
 
         console.log("Preloading UI resources");
         await ui.controls.Controls.preload();
